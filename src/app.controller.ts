7dea698c39197da74edcfdede7fd425654d674b4
badar-vulcantech <muhammad.badar@vulcantech.co>
2025-01-15 17:20:57 +0500

    Initial Setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  getPro(): string {
+    return this.appService.getHello();
+  }
+
+
 }
